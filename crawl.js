d5de3aebdf8e7a91437f99afd1348c3d9b8de726
oakinh <nikaohill@gmail.com>
2024-05-25 13:23:36 -0600

    Finish test of crawlPage, update main

--- crawl.js
+++ crawl.js
@@ -29,13 +29,33 @@ function getURLsFromHTML(html, baseURL) {
       }
     }
   }
-
   return urls
 }
 
-
-
-
+async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+  try {
+    const response = await fetch(baseURL);
+    const contentType = response.headers.get('content-type');
+    console.log('response.status =', response.status);
+    
+    if (response.status >= 400 && response.status <= 499) {
+      console.log(`400 error: ${response.status}`)
+      return;
+    }
+    else if (!contentType.includes('text/html')) {
+      console.log(`Error, wrong content-type header: ${contentType}`)
+      return;
+    }
+    else {
+      const htmlText = await response.text();
+      const dom = new JSDOM(htmlText);
+      console.log(dom.window.document.body.innerHTML)
+    }
+  } catch(err) {
+    console.log(`Error crawling page: ${err}`)
+  }
+}
 
 export { normalizeURL };
 export { getURLsFromHTML };
+export { crawlPage };
--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 import { normalizeURL } from "./crawl.js";
 import { getURLsFromHTML } from "./crawl.js";
-import { argv } from 'node:process';
-
+import { crawlPage } from "./crawl.js";
+import process from 'node:process';
+const { argv } = process;
 
 function main() {
-    argv.forEach((val, index) => {
-        console.log(`${index}: ${val}`)
-    });
+    if (argv.length > 3) {
+        console.log(`Error: Too many CLI arguments`)
+    }
+    else if (argv.length < 3) {
+        console.log(`Error: Not enough CLI arguments`)
+    }
+    else {
+        const baseURL = argv[2];
+        crawlPage(baseURL);
+        console.log(`Crawling has begun at ${baseURL}`);
+    }
 }
-
 main();
 
 
